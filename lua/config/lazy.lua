504f273448ad1492342ea5bf1561f1571f28602c
fanlizhou <fanlizhou@yunqilaohe.com>
2024-08-30 14:39:36 +0800

    chore: refactor LazyVim configuration and plugin management
    - Add `lazy-lock.json` to `.gitignore`
    - Create new `lazyvim.json` file with plugin extras and version information
    - Update `lua/config/lazy.lua` to include `dev` and `opts` options for LazyVim plugin
    - Comment out the import of custom plugins in `lua/config/lazy.lua`
    - Comment out the default colorscheme installation in `lua/config/lazy.lua`
    
    Signed-off-by: fanlizhou <fanlizhou@yunqilaohe.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,9 +17,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "doodleEsc/LazyVim", import = "lazyvim.plugins" },
+    { "doodleEsc/LazyVim", import = "lazyvim.plugins", dev = false, opts = { colorscheme = "gruvbox" } },
     -- import/override with your plugins
-    { import = "plugins" },
+    -- { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -30,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  -- install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
